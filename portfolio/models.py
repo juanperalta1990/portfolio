e49c364e672550a8144f2729246f1b88aeb982b0
Juan Manuel Peralta <jmperaltafcat@gmail.com>
2022-05-04 15:49:31 -0300

    changes model Project, description = TextField

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.db.models.fields import CharField, URLField
+from django.db.models.fields import CharField, URLField, TextField
 from django.db.models.fields.files import ImageField
 
 # Create your models here.
 
 class Project(models.Model):
     title= CharField(max_length=100)
-    description= CharField(max_length=500)
+    description= TextField(max_length=500)
     image= ImageField(upload_to='portfolio/images')
     url= URLField(blank=True)
