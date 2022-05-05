31914e1cff75a7ffee7c89fc3d4c8fbddc61224b
Juan Manuel Peralta <jmperaltafcat@gmail.com>
2022-05-05 02:11:54 -0300

    changes

--- django_portfolio/settings/prod.py
+++ django_portfolio/settings/prod.py
@@ -31,10 +31,10 @@ AWS_S3_OBJECT_PARAMETERS = {
 
 AWS_LOCATION = 'static'
 
-AWS_QUERYSTRINGS_AUTH = False
+AWS_QUERYSTRING_AUTH = False
 
 AWS_HEADERS = {
-    'Access-Control-Allow-Origin':'*',
+    'Access-Control-Allow-Origin': '*',
     }
 
 DEFAULT_FILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
