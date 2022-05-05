29c8272ef251654a5db98873d1c48a68b43fbe3b
Juan Manuel Peralta <jmperaltafcat@gmail.com>
2022-05-05 11:05:24 -0300

    changes

--- django_portfolio/settings/prod.py
+++ django_portfolio/settings/prod.py
@@ -37,6 +37,8 @@ AWS_HEADERS = {
     'Access-Control-Allow-Origin': '*',
     }
 
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+
 DEFAULT_FILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
