93404641d454aea7966b209d3ce9871f6efd3c17
Juan Manuel Peralta <jmperaltafcat@gmail.com>
2022-05-06 20:35:16 -0300

    added cv to download

--- portfolio/models.py
+++ portfolio/models.py
@@ -10,5 +10,17 @@ class Project(models.Model):
     image= ImageField(upload_to='portfolio-images/', blank=True)
     url= URLField(blank=True)
 
+    class Meta:
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
+
     def __str__(self):
+        return self.title
+
+
+class Curriculum(models.Model):
+   title = models.CharField(max_length=50)
+   file = models.FileField(upload_to="files/", null=True, blank=True)
+   
+   def __str__(self):
         return self.title
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from .models import Project
+from .models import Project, Curriculum
 # Create your views here.
 
 def home(request):
     projects= Project.objects.all()
+    curriculum= Curriculum.objects.first()
 
-    return render(request, 'home.html', {'projects': projects})
+    return render(request, 'home.html', {'projects': projects, 'curriculum': curriculum})
